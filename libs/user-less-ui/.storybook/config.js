22a58047ff4af1b6ddde3d2aefdb6951db89c484
James Dunnam <7660553+jimador@users.noreply.github.com>
2020-07-20 10:36:24 -0400

    chore: cleanup

--- libs/user-less-ui/.storybook/config.js
+++ libs/user-less-ui/.storybook/config.js
@@ -1,15 +1,11 @@
 import { addDecorator, configure } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import React from 'react';
-import { CSSReset, ThemeProvider } from '@chakra-ui/core';
 
 addDecorator (withKnobs);
 addDecorator (storyFn =>
   <>
-    <ThemeProvider>
-      <CSSReset />
       {storyFn ()}
-    </ThemeProvider>
   </>);
 addDecorator (withKnobs);
-configure (require.context ('../src/lib', true, /\.stories\.(j|t)sx?$/), module);
+configure (require.context ('../src/lib', true, /\.stories\.([jt])sx?$/), module);
