e235fc085a1201d5151fd24d30303c667eebf2fc
James Dunnam <7660553+jimador@users.noreply.github.com>
2020-07-20 11:14:38 -0400

    chore: removing chakra references

--- libs/user-less-ui/src/lib/user-less-ui.tsx
+++ libs/user-less-ui/src/lib/user-less-ui.tsx
@@ -1,33 +1,12 @@
 import React from 'react';
 
-import { Box, Text, Code, Grid, Button, ButtonGroup } from '@chakra-ui/core';
-
 export const UserLessUi = (_: {}) => {
   return (
-    <Box textAlign="center" fontSize="xl">
-      <Grid
-        minH="100vh"
-        p={3}
-      >
-        <Text>
-          <Code fontSize="xl">System.out.println("JSP FTW");</Code>.
-        </Text>
-        <ButtonGroup spacing={4}>
-          <Button variantColor="teal" variant="solid">
-            Button
-          </Button>
-          <Button variantColor="teal" variant="outline">
-            Button
-          </Button>
-          <Button variantColor="teal" variant="ghost">
-            Button
-          </Button>
-          <Button variantColor="teal" variant="link">
-            Button
-          </Button>
-        </ButtonGroup>
-      </Grid>
-    </Box>
+    <>
+      <div>
+        hello world
+      </div>
+    </>
   );
 };
 
